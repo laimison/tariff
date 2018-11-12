9cfa655973f0cc497032e63be9521e654a8386f4
Laimison <laimison1@gmail.com>
2018-11-12 05:52:51 +0000

    playing with modules and classes

--- lib/tariff.rb
+++ lib/tariff.rb
@@ -2,8 +2,22 @@ require "tariff/version"
 require 'tariff/translator'
 
 module Tariff
+  # Default is english for - Tariff.hi
+  # but it can be called with - Tariff.hi('spanish')
   def self.hi(language = "english")
     translator = Translator.new(language)
-    translator.hi       
+    translator.hi
+  end
+  
+  def hi2(language = "english")
+    translator = Translator.new(language)
+    translator.hi    
   end
 end
+
+class Laimis
+  include Tariff
+end
+
+my_test = Laimis.new
+puts my_test.hi2
