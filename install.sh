46a5fb879462aecbd17cbda6b10afe5fa0544a1d
Laimison <laimison1@gmail.com>
2018-11-12 04:37:33 +0000

    added simple test

--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gem build tariff.gemspec && gem install tariff-*.gem &&rm tariff-*.gem
--- spec/tariff_spec.rb
+++ spec/tariff_spec.rb
@@ -2,4 +2,8 @@ RSpec.describe Tariff do
   it "has a version number" do
     expect(Tariff::VERSION).not_to be nil
   end
+  
+  it 'true reports true' do
+    expect(true).to be true
+  end
 end
